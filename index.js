ed55be3c9714a2936ce811f6dd85608c9e1e2130
Miles Ager <miles.ager13@gmail.com>
2022-08-22 07:57:12 +0000

    update lodown

--- index.js
+++ index.js
@@ -10,6 +10,134 @@
  * @param {Function} action: The Function to be applied to each value in the 
  * collection
  */
+
+/**
+ * 
+ * @param {*} value
+ * @returns 
+ */
+function identity(value){
+    return value;  
+}
+module.exports.identity = identity;
+
+/**
+ * 
+ * @param {*} value
+ * @returns 
+ */
+
+function typeOf(value){
+    var t = '';
+if (Array.isArray(value)) {
+    return 'array'; 
+} 
+else if (value === null) {
+    return 'null';
+}
+else {
+    t += typeof value;
+    return t;
+}
+}
+module.exports.typeOf = typeOf;
+
+/**
+ * 
+ * @param {*} array
+ * @param {*} num
+ * @returns 
+ */
+function first(array, num){
+    // If <array> is not an array, return []
+    if (!(Array.isArray(array))) {
+return [];
+    } //If <number> is not given or not a number, return just the first element in <array>.
+    else if (typeof num !== 'number' || num === null) {
+        return array[0]; 
+    } //Otherwise, return the first <number> items of <array>
+    else { for (var i = 0; i < array.length; i++){
+        if (i > num && num < array.length) {
+            return [];
+    
+    } else {
+        return array.slice(array[0], num);
+    }
+    }
+}
+}
+module.exports.first = first;
+/**
+ * 
+ * @param {*} array
+ * @param {*} num
+ * @returns 
+ */
+
+function last(array, num){
+    // If <array> is not an array, return []
+    if (!(Array.isArray(array))) {
+return [];
+    } //If <number> is not given or not a number, return just the first element in <array>.
+    else if (typeof num !== 'number' || num === null) {
+        return array[(array.length - 1)]; 
+    } //Otherwise, return the last <number> items of <array>
+    else { for (var i = array.length; i >= 0; i--){
+        if (num > array.length) {
+            return array;
+    
+    } else {
+        return array.slice((array.length - num));
+    }
+    }
+}
+}
+module.exports.last= last;
+
+/**
+ * 
+ * @param {*} array
+ * @param {*} value
+ * @returns 
+ */
+
+function indexOf(array, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return array[i].length;
+        }
+    }
+    return -1;
+}
+
+module.exports.indexOf= indexOf;
+
+/**
+ * 
+ * @param {*} array
+ * @param {*} value
+ * @returns 
+ */
+
+function contains(array, value){
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === value){
+            return true;
+        }
+    }
+    return false;
+    }
+    
+module.exports.contains = contains;
+
+
+/**
+ * 
+ * @param {*} collection
+ * @param {*} action
+ * @returns 
+ */
+
 function each(collection, action) {
     if(Array.isArray(collection)) {
         for(var i = 0; i < collection.length; i++) {
@@ -22,6 +150,209 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+/**
+ * 
+ * @param {*} array
+ * @returns 
+ */
+
+function unique(array){
+    return [...new Set(array)];
+}
+
+module.exports.unique = unique;
+
+/**
+ * 
+ * @param {*} array 
+ * @param {*} func 
+ * @returns 
+ */
+
+function filter(array, func){
+    //looping through elements
+    var collect = [];
+for (var i = 0; i < array.length; i++){
+    //putting in function
+    if (func(array[i], i, array)){
+collect.push(array[i]);
+    }
+}
+return collect;
+}
+module.exports.filter = filter;
+
+/**
+ * 
+ * @param {*} array 
+ * @param {*} func 
+ * @returns 
+ */
+
+function reject(array, func){
+    //looping through elements
+    var collect = [];
+for (var i = 0; i < array.length; i++){
+    //putting in function if false
+    if (!func(array[i], i, array)){
+collect.push(array[i]);
+    }
+}
+return collect;
+}
+
+module.exports.reject = reject;
+
+/**
+ * 
+ * @param {*} array 
+ * @param {*} func 
+ * @returns 
+ */
+
+function partition(array, func){
+    //loop through array
+    var collect = [];
+    var collect2 = [];
+for (var i = 0; i < array.length; i++){
+    //putting in function if false
+    if (func(array[i], i, array)){
+collect.push(array[i]);
+    } else {
+collect2.push(array[i]);
+    } 
+}
+return [collect, collect2];
+}
+
+module.exports.partition = partition;
+
+/**
+ * 
+ * @param {*} collection
+ * @param {*} func 
+ * @returns 
+ */
+
+function map(collection, func) {
+    //call function for each element in collection parameter.
+    let output = [];
+if(Array.isArray(collection)){
+    for (let i = 0; i < collection.length; i++) { 
+        output.push(func(collection[i], i, collection)); 
+}
+} else { 
+    for (let key in collection) {
+        output.push(func(collection[key], key, collection));
+    }
+}
+return output;
+    //if collection is array, elemet 
+}
+
+module.exports.map = map;
+
+/**
+ * 
+ * @param {*} array
+ * @param {*} prop
+ * @returns 
+ */
+
+function pluck(array, prop){
+    //looping through array.
+return _.map(array, (element) =>{
+    return element[prop]
+});
+    
+}
+
+module.exports.pluck = pluck;
+
+/**
+ * 
+ * @param {*} collection
+ * @param {*} func
+ * @returns 
+ */
+
+function every(collection, func){
+    if (!func){
+        if (Array.isArray(collection)){
+            for (let i = 0; i < collection.length; i++){
+                if (!collection[i]){
+                    return false
+                }
+            }
+        } else {
+            for(let key in collection){
+                if (!collection[key]){
+                    return false
+                }
+            }
+        }
+    } else {
+        if (Array.isArray(collection)){
+            for (let i = 0; i < collection.length; i++){
+                if (func(collection[i], i, collection) === false){
+                    return false
+                }
+            }
+        } else {
+            for (let key in collection){
+                if (func(collection[key], key, collection) === false){
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
+
+module.exports.every = every;
+
+/**
+ * 
+ * @param {*} collection
+ * @param {*} func
+ * @returns 
+ */
+
+function some(collection, func){
+    if (!func){
+        if (Array.isArray(collection)){
+            for (let i = 0; i < collection.length; i++){
+                if (collection[i]){
+                    return true
+                }
+            }
+        } else {
+            for(let key in collection){
+                if (collection[key]){
+                    return true
+                }
+            }
+        }
+    } else {
+        if (Array.isArray(collection)){
+            for (let i = 0; i < collection.length; i++){
+                if (func(collection[i], i, collection) === true){
+                    return true
+                }
+            }
+        } else {
+            for (let key in collection){
+                if (func(collection[key], key, collection) === true){
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+
+module.exports.some = some;
 
 /**
  * 
