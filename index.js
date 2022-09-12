a04e84a85017331aced404f9ca125cb4e19b6e44
Miles Ager <miles.ager13@gmail.com>
2022-09-12 08:59:52 +0000

    lodown final revision

--- index.js
+++ index.js
@@ -12,14 +12,9 @@
  */
 
 /**
- * identity
- * takes in any value
- * returns value unchanged.
- * 
- * 
- * 
- * @param {*} value
- * @returns 
+ * identity: function returns the input value unchanged.
+ * @param {value}: function takes in any input value
+ * @returns {value}: returns the input value unchanged.
  */
 function identity(value){
     return value;  
@@ -27,13 +22,9 @@ function identity(value){
 module.exports.identity = identity;
 
 /**
- * typeOf
- * takes in any value 
- * returns the type of value as a string
- * 
- * 
- * @param {*} value
- * @returns 
+ * typeOf: function returns the data type of the input value
+ * @param {value}: function takes in any input value. 
+ * @returns {string}: returns the data type of the input value as a string
  */
 
 function typeOf(value){
@@ -52,13 +43,10 @@ else {
 module.exports.typeOf = typeOf;
 
 /**
- * first
- * takes in an array and a number
- * returns the first number item if array
- * 
- * @param {*} array
- * @param {*} num
- * @returns 
+ * first: function returns the first n number of elements from an array in an output array.
+ * @param {Array}: function takes in input array.
+ * @param {Number}: function takes in a number representing the number of elements to return from array.
+ * @return {Array}: Returns first n number of elements from an input array, etc.
  */
 function first(array, num){
     // If <array> is not an array, return []
@@ -80,13 +68,10 @@ return [];
 }
 module.exports.first = first;
 /**
- * last
- * takes in an array and a number
- * returns the last number item of array.
- * 
- * @param {*} array
- * @param {*} num
- * @returns 
+ * last: function returns the last n number of elements from an array in an output array.
+ * @param {Array}: function takes in input array.
+ * @param {Number}: function takes in a number representing the number of elements to return from array.
+ * @return {Array}: Returns last n number of elements from an input array, etc.
  */
 
 function last(array, num){
@@ -110,13 +95,10 @@ return [];
 module.exports.last= last;
 
 /**
- * indexOf
- * takes in an array and a value
- * returns the index of array that is the first occu4rance of value
- * 
- * @param {*} array
- * @param {*} value
- * @returns 
+ * indexOf: function takes in an input array and takes in a value and returns the index of the element in the array with the first occurance of the value. 
+ * @param {Array} : function takes in input array.
+ * @param {value} : function takes in a value
+ * @returns {Number} : returns an output number representing the index of the array that is the first occurance of the value 
  */
 
 function indexOf(array, value) {
@@ -131,14 +113,10 @@ function indexOf(array, value) {
 module.exports.indexOf= indexOf;
 
 /**
- * contains
- * takes in an array and a value
- * returns true if array contains value and false if not.
- * 
- * 
- * @param {*} array
- * @param {*} value
- * @returns 
+ * contains: function takes in an array and a value. returns true if array contains the value, and false if not.
+ * @param {Array}: function takes in an input array.
+ * @param {value}: function takes in a value.
+ * @returns {Boolean}: returns either true if the array conains the value, or false if array does not contain value. 
  */
 
 function contains(array, value){
@@ -154,16 +132,10 @@ module.exports.contains = contains;
 
 
 /**
- * each
- * takes in a collection and a function
- * if collection is array call function once for each element with the arguments:
- *         the element, it's index, <collection>
- * if collection is object call function once for each property with the arguments:
- *         the property's value, it's key, <collection>
- * 
- * @param {*} collection
- * @param {*} action
- * @returns 
+ * each: function takes in a collection either object or array and a function. funtion is called once for each element in array or each propery in object. * 
+ * @param {Array or Object}: takes in an input array or object
+ * @param {Function}: takes in a callback function.  passing in arguemnt array[i], i, <array> if array, and object[key], key, object if object 
+ * @returns {}: no value is returned for each, unless the callback function fed in as an argument returns a value.
  */
 
 function each(collection, action) {
@@ -180,12 +152,9 @@ function each(collection, action) {
 module.exports.each = each;
 
 /**
- * unique
- * takes in an array
- * returns a new array of all elemes from array with duplicates removed.
- * 
- * @param {*} array
- * @returns 
+ * unique: function takes in an array and returns a new array of all elements from array with duplicates removed.
+ * @param {Array}: function takes in an input array.
+ * @returns {Array}: returns a new array of all elements from array with duplicates removed.
  */
 
 function unique(array){
@@ -195,16 +164,10 @@ function unique(array){
 module.exports.unique = unique;
 
 /**
- * filter
- * takes in an array and a function
- * calls function for each element in array passing the arguments:
- *      the element, it's index, <array>
- * returns new array of elements for which caling function returned true.
- * 
- * 
- * @param {*} array 
- * @param {*} func 
- * @returns 
+ * filter: function takes in an input array and a callback function. calls function for each element in array. returns a new array that contains all the elements from the input array where the callback function returns a truethy value. 
+ * @param {Array}: function takes in an input array.
+ * @param {Function}: function takes in a callback function. passing in arguemnt array[i], i, <array>
+ * @returns {Array}: returns an array containing all elements from the input value where the callback function produced a truethy output. 
  */
 
 function filter(array, func){
@@ -221,16 +184,10 @@ return collect;
 module.exports.filter = filter;
 
 /**
- * reject
- * takes in an array and a function
- * calls function for each element in array passing the arguments:
- *      the element, it's index, <array>
- * returs a new array of elements for which calling function returned false.
- * 
- * 
- * @param {*} array 
- * @param {*} func 
- * @returns 
+ * reject: function that takes in an array and a function and calls function for each element in array. returns new array of elements for which caling function returned false.
+ * @param {Array}: function takes in an input array.
+ * @param {Function}: function takes in a callback function. passing in arguemnt array[i], i, <array>
+ * @returns {Array}: returns a new array that contains all the elements from the input array for which the callback function resulted in a falsey output.
  */
 
 function reject(array, func){
@@ -248,17 +205,10 @@ return collect;
 module.exports.reject = reject;
 
 /**
- * partition
- * takes in an array and a function
- * calls function for each element in array passing it the arguments:
- *       element, key, <array>
- *  returns an array made up of two sub arrays.
- * 0) An array that contains all the values for which <function> returned something truthy
- *  1) An array that contains all the values for which <function> returned something falsy
- * 
- * @param {*} array 
- * @param {*} func 
- * @returns 
+ * partition: function that takes in an input array and a callback function. calls the function for each element in array.
+ * @param {Array}: function takes in an input array.
+ * @param {Function}: function takes in a callback function. passing in arguemnt array[i], i, <array>
+ * @returns {Array}: returns two new arrays. One that contains all the elements from the input array for which the callback function resulted in a truethy output and an array for all the elements that the callbac function resulted in a falsey output.
  */
 
 function partition(array, func){
@@ -279,18 +229,10 @@ return [collect, collect2];
 module.exports.partition = partition;
 
 /**
- * map
- * takes in a collection and a function.
- * calls function for each element in collection pasing the arguments
- * if <collection> is an array:
- *            the element, it's index, <collection>
- *        if <collection> is an object:
- *            the value, it's key, <collection>
- * returns new array of return values for each function call.
- * 
- * @param {*} collection
- * @param {*} func 
- * @returns 
+ * map: function takes in an input collection, either object or array, and a callback function. calls function for each elemnt in collection and returns a new array of values produced from function being called on each element.  
+ * @param {Array or Object}: takes in an input array or object
+ * @param {Function}: takes in a callback function.  passing in arguemnt array[i], i, <array> if array, and object[key], key, object if object 
+ * @returns {Array}: returns a new array of return values for each function call.
  */
 
 function map(collection, func) {
@@ -312,13 +254,10 @@ return output;
 module.exports.map = map;
 
 /**
- * pluck
- * takes in an array and object
- * returns and array containg the value of of <property> for every element in <array>
- * 
- * @param {*} array
- * @param {*} prop
- * @returns 
+ * pluck: function takes in an input array and a value representing an object property. returns an array containing the value for each object property within the elements of the input array.
+ * @param {Array}: function takes in an input array
+ * @param {value}: function takes in a value representing an object property.
+ * @returns {Array}: returns an array containg the value of of the object property for every element in the input array 
  */
 
 function pluck(array, prop){
@@ -332,20 +271,10 @@ return _.map(array, (element) =>{
 module.exports.pluck = pluck;
 
 /**
- * every
- * takes in a collection and a function 
- * all <function> for every element of <collection> with the paramaters:
- *      if <collection> is an array:
- *          current element, it's index, <collection>
- *      if <collection> is an object:
- *          current value, current key, <collection>
- * If the return value of calling <function> for every element is true, return true
- *   If even one of them returns false, return false
- *   If <function> is not provided, return true if every element is truthy, otherwise return false
- * 
- * @param {*} collection
- * @param {*} func
- * @returns 
+ * every: function takes in an input collection, either an array or an object, and takes in a callback function. calls function for every element of the collection. if the return value for every element is true, return true. otherwise, return false.
+ * @param {Array or Object}: takes in an input array or object
+ * @param {Function}: takes in a callback function.  passing in arguemnt array[i], i, <array> if array, and object[key], key, object if object 
+ * @returns {Boolean}: returns a boolean value of true if every element in the input array returns true with the callback function, else the function will return the boolean value of false.
  */
 
 function every(collection, func){
@@ -384,20 +313,10 @@ function every(collection, func){
 module.exports.every = every;
 
 /**
- * some
- * takes a collection and a function
- * Call <function> for every element of <collection> with the paramaters:
-*       if <collection> is an array:
-*        current element, it's index, <collection>
-*       if <collection> is an object:
-*        current value, current key, <collection>
- * If the return value of calling <function> is true for at least one element, return true
-*   If it is false for all elements, return false
-*   If <function> is not provided return true if at least one element is truthy, otherwise return false
- * 
- * @param {*} collection
- * @param {*} func
- * @returns 
+ * some: function takes an input collection, either an array or an object, and takes a callback function. calls function for every element in the array. if the outcome of calling the callback function on every element returns at least one true value, we some returns true. else some returns false.  
+ * @param {Array or Object}: takes in an input array or object
+ * @param {Function}: takes in a callback function.  passing in arguemnt array[i], i, <array> if array, and object[key], key, object if object 
+ * @returns {Boolean}: returns a boolean value of true if at least one element in the input array returns true with the callback function, else the function will return the boolean value of false.
  */
 
 function some(collection, func){
@@ -436,21 +355,11 @@ function some(collection, func){
 module.exports.some = some;
 
 /**
- * reduce 
- * takes in an array a function and a seed 
- * 
- *  Call <function> for every element in <collection> passing the arguments:
- *         previous result, element, index
- *   Use the return value of <function> as the "previous result"
- *      for the next iteration
- *   On the very first iteration, use <seed> as the "previous result"
- *   If no <seed> was given, use the first element/value of <collection> as <seed> and continue to the next element
- *   After the last iteration, return the return value of the final <function> call
- * 
- * @param {*} array 
- * @param {*} func 
- * @param {*} seed 
- * @returns 
+ * reduce: function takes in an input array, a callback function, and a seed (previous result). calls function for every element in array. the callback function will pass in the arguement's previous result (or the seed), element, index. After the last iteration, the value of the final function call is returned.
+ * @param {Array}: function takes in an input array.
+ * @param {Function}: function takes in a callback function. passing in arguemnt previous value, current value, index
+ * @param {seed}: function takes in a seed as one of the arguments in the callback function 
+ * @returns {value}: returns the value produced by the final function call. 
  */
 function reduce(array, func, seed){
     //looping through collection
@@ -475,9 +384,9 @@ function reduce(array, func, seed){
     module.exports.reduce = reduce;
 
 /**
- * 
- * @param {*} object 
- * @returns 
+ * extend: function takes in a target object and any number of additional objects. Copies all properties from all objects and adds them to the target object, and returns the updated object. 
+ * @param {Object}: function takes in a target object and any number of additional objects. 
+ * @returns {Object}: returns the updated target object with all properties from all additional objects concatenated to the target object. 
  */
 
     function extend(object){
